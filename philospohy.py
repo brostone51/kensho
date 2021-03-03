02f4aa0b1167bb36bbf1336816e1753706f8ec04
Joe <joe.stone.se@gmail.com>
2021-03-02 19:21:02 -0500

    add tracking to optimize pruning of the search

--- philospohy.py
+++ philospohy.py
@@ -20,17 +20,25 @@ def get_first_anchor(soup: BeautifulSoup):
   """ Gets first link from a page and retuns the tag of it
   """
   body = soup.find(name='div', id='bodyContent')
-  for p in soup.find_all(name='p'):
+  for p in soup.find_all(name=['p', 'ul']):
     for a in p.find_all(name='a', href=re.compile('^(/wiki/).*$')):
       if a.text and a.text[0] != '(':
         return a
   return None
 
-def find_philosphy(url: str) -> (bool, int):
+leads_to_pass = set()
+leads_to_fail = set()
+def find_philosphy(url: str) -> (bool, int, set):
   visited = set()
   while url not in visited:
     print(f"Visiting {url}...")
     visited.add(url)
+    if url in leads_to_fail:
+        print(f'We know where {url} leads us... :(')
+        return (False, 0, visited)
+    if url in leads_to_pass:
+      print(f'We know where {url} leads us!!! :)')
+      return (True, len(visited), visited)
     page = get_wikipedia_page(url)
     anchor = get_first_anchor(page)
     if anchor == None: return (False, 0, visited)
@@ -42,12 +50,15 @@ pass_count = 0
 fail_count = 0
 max_path_len = -1
 for i in range(100):
-  result, count = find_philosphy(random_url())
+  result, count, visited = find_philosphy(random_url())
   if result:
     pass_count += 1
     max_path_len = max(count, max_path_len)
+    leads_to_pass = leads_to_pass | visited
   else:
     fail_count += 1
+    leads_to_fail = leads_to_fail | visited
 
 print('Passes: ', pass_count)
 print('Fails: ', fail_count)
+print('Longest path: ', max_path_len)
