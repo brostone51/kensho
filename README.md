a53414e6abb9f189e3ffbad766673bb36a9b8b53
Joe <joe.stone.se@gmail.com>
2021-03-02 20:03:33 -0500

    update readme and link parsing function

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
 # kensho
 
-As of my push at ~7:30, it parses through the apges super well and arrives at results pretty quickly, even in my VM where the process is a bit limited.
+As of my push at ~7:30, it parses through the pages super well and arrives at results pretty quickly, even in my VM where the process is a bit limited.
 However, the results aren't matching what the wikipedia page says should happen. Something is likely to be wrong with my `get_first_anchor` function.
+
+### update
+
+Was able to make it better, but only somewhat. There seems to be something wrong with my link finding function, but I'm not sure what. According to the sample pages I am actually viewing, it should be accurately grabbing the first link from the page. But based on the ratio I'm seeing in outuput, either this article is no longer true or something is up.
+
+At any rate, I am pretty much out of time here. I wish I could figure out why I'm getting the wrong link, but it still succesffuly parses the link chains and does some pruning when it knows it going to fail.
+
+Here is a sample output:
+
+```text
+
+```
--- philospohy.py
+++ philospohy.py
@@ -1,3 +1,4 @@
+import collections
 import re
 
 import requests
@@ -17,48 +18,68 @@ def get_wikipedia_page(url: str) -> BeautifulSoup:
   return soup
 
 def get_first_anchor(soup: BeautifulSoup):
-  """ Gets first link from a page and retuns the tag of it
+  """ Gets first link from a page and returns the tag of it
   """
-  body = soup.find(name='div', id='bodyContent')
-  for p in soup.find_all(name=['p', 'ul']):
-    for a in p.find_all(name='a', href=re.compile('^(/wiki/).*$')):
+  body = soup.find(name='div', class_='mw-parser-output')
+  for p in body.find_all(name='p', recursive=False):
+    for a in p.find_all(name='a', href=re.compile('^(/wiki/).*$'), recursive=False):
       if a.text and a.text[0] != '(':
         return a
   return None
 
-leads_to_pass = set()
 leads_to_fail = set()
-def find_philosphy(url: str) -> (bool, int, set):
+def find_philosphy(url: str) -> (bool, int, set, list):
+  path = []
   visited = set()
   while url not in visited:
-    print(f"Visiting {url}...")
+    # Update visited set and path to this url
+    path.append(url.split('/')[-1])
     visited.add(url)
+
     if url in leads_to_fail:
-        print(f'We know where {url} leads us... :(')
-        return (False, 0, visited)
-    if url in leads_to_pass:
-      print(f'We know where {url} leads us!!! :)')
-      return (True, len(visited), visited)
+      # Check if we know this path leads to failure already
+      return (False, 0, visited, path)
+
+    # Get the page from wikipedia and find the first link
     page = get_wikipedia_page(url)
     anchor = get_first_anchor(page)
-    if anchor == None: return (False, 0, visited)
-    if anchor.get('href') == '/wiki/Philosophy': return (True, len(visited), visited)
+
+    if anchor == None: 
+      # The page, suprisingly, has no links
+      return (False, 0, visited, path)
+
+    if anchor.get('href') == '/wiki/Philosophy':
+      # First link is philosophy
+      path.append('Philosophy')
+      return (True, len(path) + 1, visited, path)
+
+    # Set url for next iterator
     url = f"https://en.wikipedia.org{anchor.get('href')}"
-  return (False, 0, visited)
+
+  # We entered a loop
+  print('Loop ', end='')
+  path.append(url.split('/')[-1])
+  return (False, 0, visited, path)
 
 pass_count = 0
 fail_count = 0
 max_path_len = -1
+last_page_frequencies = collections.defaultdict(int)
 for i in range(100):
-  result, count, visited = find_philosphy(random_url())
+  result, count, visited, path = find_philosphy(random_url())
+  last_page_frequencies[path[-1]] += 1
   if result:
     pass_count += 1
     max_path_len = max(count, max_path_len)
-    leads_to_pass = leads_to_pass | visited
+    print('~~~ Path Found: ', ' -> '.join(path))
   else:
     fail_count += 1
     leads_to_fail = leads_to_fail | visited
+    print('Failure: ', ' -> '.join(path))
 
 print('Passes: ', pass_count)
 print('Fails: ', fail_count)
 print('Longest path: ', max_path_len)
+print('\nFrequencies of the last page in the chain:')
+for k,v in last_page_frequencies.items():
+  print(k, ': ', v)
